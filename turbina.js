5c6cded2a9fe3e0cdf11fa2ae228c2c60916eaa8
Victor <victoraweb@gmail.com>
2017-04-28 12:02:39 -0300

    [ FEATURE ] Tasks resource type added.

--- turbina.js
+++ turbina.js
@@ -1,12 +1,29 @@
 $(function() {
-    $('#go-process-instance').click(function() {
-        var procInst = $('#process-instance-input');
+    $('.go-button').click(function() {
+        var closestInputId = $(this).attr('for');
+        var procInst = $(`#${closestInputId}`);
+        var resourceType;
         if(!procInst.val()) {
             procInst.focus();
             procInst.addClass('error');
             return false;
         }
         procInst.removeClass('error');
-        window.open(`https://demodev.icolabora.com.br/#processinstances/${procInst.val()}`, '_blank')
+
+        switch($(this).attr('type')) {
+            case 'processinstance': {
+                resourceType = 'processinstances';
+                break;
+            }
+            case 'task': {
+                resourceType = 'tasks';
+                break;
+            }
+            default: {
+                alert('No resource type provided.')
+                return false;
+            }
+        }
+        window.open(`https://demodev.icolabora.com.br/#${resourceType}/${procInst.val()}`, '_blank')
     });
 });
