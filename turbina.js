65ce0ddb2b4ff59f341fe6d57d127563de37cb8f
Victor <victoraweb@gmail.com>
2017-04-28 19:37:46 -0300

    [ FEATURE ] Adding new environments but not persisting yet.

--- turbina.js
+++ turbina.js
@@ -2,6 +2,7 @@ $(function() {
     $('.go-button').click(function() {
         var closestInputId = $(this).attr('for');
         var procInst = $(`#${closestInputId}`);
+        var environment = $(`#environments-select option[value=${$('#environments-select').val()}]`);
         var resourceType;
         if(!procInst.val()) {
             procInst.focus();
@@ -24,6 +25,63 @@ $(function() {
                 return false;
             }
         }
-        window.open(`https://demodev.icolabora.com.br/#${resourceType}/${procInst.val()}`, '_blank')
+        var url = `${environment.attr('baseurl')}/#${resourceType}/${procInst.val()}`.replace(/(?![\w\d])\/\/+/g, '/'); // Remove excessive bars '/'
+        window.open(url, '_blank')
     });
+
+    function slideToggleContainers() {
+        $('#new-environment-container').slideToggle();
+        $('#actions-container').slideToggle();
+    }
+
+    // NEW ENVIRONMENT
+    $('#add-environment-btn').click(function() {
+        $('#add-environment-btn').toggleClass('toggled-button');
+        slideToggleContainers();
+    });
+
+    $('#ok-new-env-btn').click(function() {
+        var envName    = $('#environment-name-input');
+        var envNameVal = envName.val().replace(/ /g, '').toUpperCase();
+        var envBaseUrl = $('#environment-baseurl-input');
+        
+        if(!envNameVal) {
+            envName.addClass('error');
+            return false;
+        } else {
+            envName.removeClass('error');
+        }
+
+        if(!envBaseUrl.val()) {
+            envBaseUrl.addClass('error');
+            return false;
+        } else {
+            envBaseUrl.removeClass('error');
+        }
+
+        var newOption = $('<option/>', {
+            value  : envNameVal,
+            baseurl: envBaseUrl.val().toUpperCase(),
+            text   : envName.val().toUpperCase()
+        });
+
+        $('#environments-select').append(newOption);
+        $('#environments-select').val(envNameVal);
+        slideToggleContainers()
+
+    });
+
+    $('#environment-name-input, #environment-baseurl-input').change(function() {
+        var envName    = $('#environment-name-input');
+        var envBaseUrl = $('#environment-baseurl-input');
+        
+        if(envName.val()) {
+            envName.removeClass('error');
+        }
+
+        if(envBaseUrl.val()) {
+            envBaseUrl.removeClass('error');
+        }
+    });
+    
 });
