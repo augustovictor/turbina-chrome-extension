8533742bc20afa1ea41d0a3032b20f4305bb9d33
Victor <victoraweb@gmail.com>
2017-04-28 00:47:57 -0300

    [ FEATURE ] Opening tab with given parameter

--- Turbina.js
+++ Turbina.js
@@ -1,37 +1,6 @@
-var base_url = "https://demodev.icolabora.com.br/#processinstances/";
-
-function sendToTurbinaPage() {
-    chrome.tabs.executeScript(null, {
-        code: `
-            var processInstanceInput = document.getElementById('process-instance-input');
-            console.log(document);
-            // var url = 'http://demodev.icolabora.com.br/#processinstances/' + processInstanceInput.value;
-            // var win = window.open(url, '_blank');
-            // win.focus();
-        `
+$(function() {
+    $('#go-process-instance').click(function() {
+        var procInst = $('#process-instance-input').val();
+        window.open(`https://demodev.icolabora.com.br/${procInst}`, '_blank')
     });
-    // window.close()
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    // var processInstanceInput = document.getElementById('process-instance-input');
-    var processInstanceBtn = document.getElementById('go-process-instance');
-
-    processInstanceBtn.addEventListener('click', function() {
-        chrome.tabs.executeScript(null, {
-            code: `
-                console.log('foi');
-                var processInstanceInput = document.getElementById('process-instance-input');
-                console.log(processInstanceInput.value);
-            
-            `
-        })
-
-    });
-
-
-    // var goButtons = document.querySelectorAll('button');
-    // for(var i = 0; i < goButtons.length; i++) {
-    //     goButtons[i].addEventListener('click', sendToTurbinaPage);
-    // }
 });
