c2e50973b3da13d048ae282a0af998a273724f63
Victor <victoraweb@gmail.com>
2017-04-28 01:22:34 -0300

    [ FEATURE ] Input and action button inline

--- Turbina.js
+++ Turbina.js
@@ -1,6 +1,12 @@
 $(function() {
     $('#go-process-instance').click(function() {
-        var procInst = $('#process-instance-input').val();
-        window.open(`https://demodev.icolabora.com.br/${procInst}`, '_blank')
+        var procInst = $('#process-instance-input');
+        if(!procInst.val()) {
+            procInst.focus();
+            procInst.addClass('error');
+            return false;
+        }
+        procInst.removeClass('error');
+        window.open(`https://demodev.icolabora.com.br/#processinstances/${procInst.val()}`, '_blank')
     });
 });
